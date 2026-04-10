ba2b4fa262c6690120f9d8f11669f0810d6df29b
francis85a <franperezcid85@gmail.com>
2026-04-10 12:30:57 +0200

    build(bici_palma): Crear proyecto

---
+++ src/main/java/edu/teamrocket/bici_palma/main/BiciPalma.java
@@ -0,0 +1,10 @@
+package edu.teamrocket.bici_palma.main;
+
+/**
+ * Hello world!
+ */
+public class BiciPalma {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
